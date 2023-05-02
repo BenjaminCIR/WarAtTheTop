289fe896b93fb8a33acc45f49a63246c3e220deb
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-05-02 17:57:03 +0200

    feur

--- inscription.php
+++ inscription.php
@@ -59,7 +59,7 @@
                             <div class="check">
                                 <label>				
                                     <input id="check" type="checkbox" class="checkbox">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="23px">
+                                        <svg width="26px" height="23px">
                                             <path class="path-back"  d="M1.5,6.021V2.451C1.5,2.009,1.646,1.5,2.3,1.5h18.4c0.442,0,0.8,0.358,0.8,0.801v18.398c0,0.442-0.357,0.801-0.8,0.801H2.3c-0.442,0-0.8-0.358-0.8-0.801V6"/>
                                             <path class="path-moving" d="M24.192,3.813L11.818,16.188L1.5,6.021V2.451C1.5,2.009,1.646,1.5,2.3,1.5h18.4c0.442,0,0.8,0.358,0.8,0.801v18.398c0,0.442-0.357,0.801-0.8,0.801H2.3c-0.442,0-0.8-0.358-0.8-0.801V6"/>
                                         </svg>
