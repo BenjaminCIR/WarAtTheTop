a8a8d006afb165d9a689b652b7ddd5d8f5df56bc
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-05-09 17:24:36 +0200

    feur

--- opwatt.sql
+++ opwatt.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 02 mai 2023 à 10:13
+-- Généré le : mar. 09 mai 2023 à 15:24
 -- Version du serveur :  5.7.24
--- Version de PHP : 8.0.1
+-- Version de PHP : 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,15 +33,25 @@ CREATE TABLE `users` (
   `pseudo` text NOT NULL,
   `password` text NOT NULL,
   `cards` text NOT NULL,
-  `decks` text NOT NULL
+  `decks` text NOT NULL,
+  `money` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `pack standard` int(11) NOT NULL,
+  `pack rare` int(11) NOT NULL,
+  `pack légendaire` int(11) NOT NULL,
+  `pack north blue` int(11) NOT NULL,
+  `pack south blue` int(11) NOT NULL,
+  `pack east blue` int(11) NOT NULL,
+  `pack west lue` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `pseudo`, `password`, `cards`, `decks`) VALUES
-(1, 'EVAWDUNOS', 'admin', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755,8,2661,2804,3275,3048,1593', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755;');
+INSERT INTO `users` (`id`, `pseudo`, `password`, `cards`, `decks`, `money`, `email`, `pack standard`, `pack rare`, `pack légendaire`, `pack north blue`, `pack south blue`, `pack east blue`, `pack west lue`) VALUES
+(1, 'EVAWDUNOS', 'admin', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755,8,2661,2804,3275,3048,1593', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755', 99999, '', 0, 0, 0, 0, 0, 0, 0),
+(2, 'rere', 'rere', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755,8,2661,2804,3275,3048,1593', '3786,1413,3226,3609,2700,1707,3720,3812,2018,2381,2964,1267,2363,3672,3755', 99999, 'rem.degraeve@gmail.com', 0, 0, 0, 0, 0, 0, 0);
 
 --
 -- Index pour les tables déchargées
@@ -60,7 +71,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
