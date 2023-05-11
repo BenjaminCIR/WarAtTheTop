387aa1975af0cbf0fcca7fe57fb8b87ad04bc4ed
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-05-11 15:28:46 +0200

    feur

---
+++ shop.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Boutique</title>
+    <link rel="stylesheet" href="shop.css">
+    <script src="shop.js"></script>
+
+  </head>
+  <body>
+    <div class="background"></div>
+    <div class="content">
+      <img id="img1" src="img1.jpg">
+      <a href="#" id="scrollToTopButton" class="scroll-to-top-button">
+        <img src="flèche.jpg" alt="Retour en haut de la page">
+      </a>
+
+      <div class="container">
+        <div class="block">
+          <img src="pack1.jpg" alt="Image 1" onclick="scrollToBloc2()">
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="block">
+          <img src="pack1.jpg" alt="Image 2" onclick="scrollToBloc3()">
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="block">
+          <img src="pack1.jpg" alt="Image 3" onclick="scrollToBloc4()">
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="block">
+          <img src="pack1.jpg" alt="Image 4"onclick="scrollToBloc5()">
+        </div>
+      </div>
+
+      <div class="container1">
+        <div class="left">
+          <div class="block0">
+            <img src="pack1.jpg" alt="Image 1" onclick="scrollToBloc6()">
+          </div>
+        </div>
+      </div>
+
+      <div class="container1">
+        <div class="block0">
+          <img src="pack1.jpg" alt="Image 1" onclick="scrollToBloc7()">
+        </div>
+      </div>
+
+      <div class="container1">
+        <div class="block0">
+          <img src="pack1.jpg" alt="Image 1" onclick="scrollToBloc8()">
+        </div>
+      </div>
+      
+      <div class="cards">
+        <div class="card"  id="bloc2">
+          <h1>North Blue</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc3">
+          <h1>South Blue</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc4">
+          <h1>East Blue</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc5">
+          <h1>West Blue</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc6">
+          <h1>Pack Rare</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc7">
+          <h1>Pack Epique</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+        <div class="card" id="bloc8">
+          <h1>Pack Légendaire</h1>
+          <div class="cont">
+            <div class="coffre">
+              <img class="left-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 1 Pack</button>
+            </div>
+            <div class="coffre">
+              <img class="center-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 2 Packs</button>
+            </div>
+            <div class="coffre">
+              <img class="right-image" src="pack1.jpg">
+              <button class="petit-bouton">Acheter 3 Packs</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
